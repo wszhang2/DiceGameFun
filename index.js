b04f5135473b6c04bec686b3d5d73a80071cfeb3
wszhang2 <wszhang2@illinois.edu>
2026-01-13 10:24:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var randomNum1 =Math.floor( Math.random()*6)+1;
-var randomDiceImage1 = "Dice"+randomNum1+".png";
+var randomDiceImage1 = "dice"+randomNum1+".png";
 
 var imageSource1 = "images/"+ randomDiceImage1;
 var image1= document.querySelectorAll("img")[0];
 image1.setAttribute("src",imageSource1);
 
 var randomNum2 =Math.floor( Math.random()*6)+1;
-var randomDiceImage2 = "Dice"+randomNum2+".png";
+var randomDiceImage2 = "dice"+randomNum2+".png";
 var imageSource2 = "images/"+ randomDiceImage2;
 var image2= document.querySelectorAll("img")[1];
 image2.setAttribute("src",imageSource2);
@@ -23,4 +23,5 @@ else if(randomNum1==randomNum2)
 else 
     {
     document.querySelector("h1").innerHTML="🚩Play 2 Wins!";
+
 }
